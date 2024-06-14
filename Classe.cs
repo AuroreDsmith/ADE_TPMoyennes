07f345b4eb7ceaf6096004ba9e61904baab9e71f
AuroreDsmith <aurore.desmis@gmail.com>
2024-06-14 11:06:30 +0200

    creation class Eleve et Classe

---
+++ Classe.cs
@@ -0,0 +1,18 @@
+﻿using TPMoyennes;
+using (Eleve);
+
+namespace TPMoyennes
+{
+    internal class Classe
+    {
+        
+        public string nomClasse { get; private set; }
+
+        public Classe(string nomClasse)
+        {
+            this.nomClasse = nomClasse;
+        }
+
+
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,17 @@
+﻿using TPMoyennes;
+using (Note);
+
+namespace TPMoyennes
+{
+    internal class Eleve
+    {
+        public string prenom { get; private set; }
+        public string nom { get; private set; }
+
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+        }
+    }
+}
